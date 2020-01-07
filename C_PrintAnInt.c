452a9bf18914bba2fb45fe4046959b080e2231ad
Ubuntu <machowdhury1@gmail.com>
2020-01-07 02:17:52 +0000

    This is version v1.3

---
+++ C_PrintAnInt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+
+int number;
+printf ("Enter an integer: ");
+
+//reads and stores input
+scanf("%d", &number);
+
+//display output
+printf("You enterd: %d", number);
+
+return 0;
+
+}
